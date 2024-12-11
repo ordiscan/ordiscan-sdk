f8c18a9be74c115779b169734bf65df1d7ee14d5
Tristan (t4t5) <t4t5@hey.com>
2024-12-11 18:00:34 +0000

    Use schemas for tx types

--- src/resources/tx.ts
+++ src/resources/tx.ts
@@ -1,75 +1,93 @@
+import { z } from "zod";
+
 import { Ordiscan } from "../client";
 import { Inscription } from "./inscription";
 
-interface RunestoneMessage {
-  rune: string;
-  type: "TRANSFER" | "MINT" | "ETCH";
-}
+export const RunestoneMessageSchema = z.object({
+  rune: z.string(),
+  type: z.enum(["TRANSFER", "MINT", "ETCH"]),
+});
 
-interface RunicInput {
-  address: string;
-  output: string;
-  rune: string;
-  rune_amount: string;
-}
+export type RunestoneMessage = z.infer<typeof RunestoneMessageSchema>;
 
-interface RunicOutput {
-  address: string;
-  vout: number;
-  rune: string;
-  rune_amount: string;
-}
+export const RunicInputSchema = z.object({
+  address: z.string(),
+  output: z.string(),
+  rune: z.string(),
+  rune_amount: z.string(),
+});
 
-export interface RunicTx {
-  txid: string;
-  runestone_messages: RunestoneMessage[];
-  inputs: RunicInput[];
-  outputs: RunicOutput[];
-  timestamp: string; // TODO: change?
-}
+export type RunicInput = z.infer<typeof RunicInputSchema>;
 
-export interface InscriptionTransfer {
-  from_address: string;
-  to_address: string;
-  inscription_id: string;
-  spent_as_fee: boolean;
-  txid: string;
-  confirmed: boolean;
-  timestamp: string; // TODO: change to date?
-}
+export const RunicOutputSchema = z.object({
+  address: z.string(),
+  vout: z.number(),
+  rune: z.string(),
+  rune_amount: z.string(),
+});
 
-export interface InscriptionActivity {
-  txid: string;
-  type: "INSCRIBE" | "RECEIVE" | "SEND";
-  inscription_id: string;
-  counterpart_address: string | null;
-  spent_as_fee: boolean;
-  confirmed: boolean;
-  timestamp: string;
-}
+export type RunicOutput = z.infer<typeof RunicOutputSchema>;
 
-export interface Brc20Activity {
-  ticker: string;
-  type: "TRANSFER" | "MINT" | "DEPLOY";
-  from_address: string | null;
-  to_address: string | null;
-  amount: number;
-  inscription_id: string;
-  timestamp: string;
-}
+export const RunicTxSchema = z.object({
+  txid: z.string(),
+  runestone_messages: z.array(RunestoneMessageSchema),
+  inputs: z.array(RunicInputSchema),
+  outputs: z.array(RunicOutputSchema),
+  timestamp: z.string(),
+});
 
-export interface TxInfo {
-  txid: string;
-  fee: number;
-  size: number;
-  weight: number;
-  confirmed: boolean;
-  block_hash: string;
-  indexed: boolean;
-  has_inscriptions: boolean;
-  has_inscription_transfers: boolean;
-  has_runes: boolean;
-}
+export type RunicTx = z.infer<typeof RunicTxSchema>;
+
+export const InscriptionTransferSchema = z.object({
+  from_address: z.string(),
+  to_address: z.string(),
+  inscription_id: z.string(),
+  spent_as_fee: z.boolean(),
+  txid: z.string(),
+  confirmed: z.boolean(),
+  timestamp: z.string(),
+});
+
+export type InscriptionTransfer = z.infer<typeof InscriptionTransferSchema>;
+
+export const InscriptionActivitySchema = z.object({
+  txid: z.string(),
+  type: z.enum(["INSCRIBE", "RECEIVE", "SEND"]),
+  inscription_id: z.string(),
+  counterpart_address: z.string().nullable(),
+  spent_as_fee: z.boolean(),
+  confirmed: z.boolean(),
+  timestamp: z.string(),
+});
+
+export type InscriptionActivity = z.infer<typeof InscriptionActivitySchema>;
+
+export const Brc20ActivitySchema = z.object({
+  ticker: z.string(),
+  type: z.enum(["TRANSFER", "MINT", "DEPLOY"]),
+  from_address: z.string().nullable(),
+  to_address: z.string().nullable(),
+  amount: z.number(),
+  inscription_id: z.string(),
+  timestamp: z.string(),
+});
+
+export type Brc20Activity = z.infer<typeof Brc20ActivitySchema>;
+
+export const TxInfoSchema = z.object({
+  txid: z.string(),
+  fee: z.number(),
+  size: z.number(),
+  weight: z.number(),
+  confirmed: z.boolean(),
+  block_hash: z.string(),
+  indexed: z.boolean(),
+  has_inscriptions: z.boolean(),
+  has_inscription_transfers: z.boolean(),
+  has_runes: z.boolean(),
+});
+
+export type TxInfo = z.infer<typeof TxInfoSchema>;
 
 export class Tx {
   constructor(
